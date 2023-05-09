4ee88b3fbb600fd70f819e4e2fd34de7a5101414
aa452948257 <452948257@qq.com>
2023-05-09 15:23:58 +0800

    stock benchmark

--- Stock/StockVP.py
+++ Stock/StockVP.py
@@ -20,12 +20,12 @@ def stock_benchmark():
     x_stocks_df = xpd.read_csv(data_path, chunk_bytes=200 * 1024 * 1024).iloc[:10000]
     xorbits.run(x_stocks_df)
 
-    # 计算股票价格的移动平均线
+    # Calculating Moving Averages for Stock Prices
     start_time = time.time()
     p_stock_mean_df = p_stocks_df.groupby(
         ['StockName']).rolling(window=10).mean(numeric_only=True)[['Open', 'High', 'Low', 'Close', 'Volume']]
     end_time = time.time()
-    print("Pandas 计算股票价格的移动平均线 Execution time: ", end_time - start_time)
+    print("Pandas Calculating Moving Averages for Stock Prices Execution time: ", end_time - start_time)
 
     start_time = time.time()
     x_stock_mean_df = x_stocks_df.groupby(
@@ -33,13 +33,13 @@ def stock_benchmark():
     ).apply(lambda x: x.rolling(window=10).mean(numeric_only=True))[['Open', 'High', 'Low', 'Close', 'Volume']]
     xorbits.run(x_stock_mean_df)
     end_time = time.time()
-    print("Xorbits 计算股票价格的移动平均线 Execution time: ", end_time - start_time)
+    print("Xorbits Calculating Moving Averages for Stock Prices Execution time: ", end_time - start_time)
 
-    # 计算收益率
+    # Returns
     start_time = time.time()
     p_stocks_df['Ret'] = p_stocks_df[['StockName', 'Close']].groupby(['StockName']).pct_change()
     end_time = time.time()
-    print("Pandas 计算收益率 Execution time: ", end_time - start_time)
+    print("Pandas Returns Execution time: ", end_time - start_time)
 
     start_time = time.time()
     x_stocks_ret_df = x_stocks_df[['StockName', 'Close']].groupby(
@@ -50,13 +50,13 @@ def stock_benchmark():
     x_stocks_df['Ret'] = x_stocks_ret_df['Close']
     xorbits.run(x_stocks_ret_df)
     end_time = time.time()
-    print("Xorbits 计算收益率 Execution time: ", end_time - start_time)
+    print("Xorbits Returns Execution time: ", end_time - start_time)
 
-    # 计算股票价格的移动年化波动率
+    # Calculating the Moving Annualized Volatility of Stock Returns
     start_time = time.time()
     p_Volatility = p_stocks_df[['StockName', 'Ret']].groupby(['StockName']).rolling(window=10).std() * np.sqrt(252)
     end_time = time.time()
-    print("Pandas 计算股票价格的移动年化波动率 Execution time: ", end_time - start_time)
+    print("Pandas Calculating the Moving Annualized Volatility of Stock Returns Execution time: ", end_time - start_time)
 
     start_time = time.time()
     x_Volatility = x_stocks_df[['StockName', 'Ret']].groupby(
@@ -65,7 +65,7 @@ def stock_benchmark():
 
     xorbits.run(x_Volatility)
     end_time = time.time()
-    print("Xorbits 计算股票价格的移动年化波动率 Execution time: ", end_time - start_time)
+    print("Xorbits Calculating the Moving Annualized Volatility of Stock Returns Execution time: ", end_time - start_time)
 
     # Window Normalization
     start_time = time.time()
@@ -83,9 +83,9 @@ def stock_benchmark():
     end_time = time.time()
     print("Xorbits Window Normalization Execution time: ", end_time - start_time)
 
-    # 测试两个结果是否相同
+    # Test equal
     pd.testing.assert_frame_equal(p_stocks_df, x_stocks_df.to_pandas())
-    print("Xorbits, Pandas, 计算结果结果相同")
+    print("Xorbits, Pandas, The same result")
 
 
 if __name__ == '__main__':
